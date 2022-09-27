5a26b797e36aa0da82ec83fc348d6cdad7d08204
TanLeSi <tan.lesi@study.hs-duesseldorf.de>
2022-09-27 15:46:39 +0200

    update config

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 
 
-# echo "\
-# [server]\n\
-# headless = true\n\
-# enableCORS=false\n\
-# port = $PORT\n\
-# address = 0.0.0.0\n\
-# \n\
-# " > ~/.streamlit/config.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+\n\
+" > ~/.streamlit/config.toml
