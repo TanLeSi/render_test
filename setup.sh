ee74fd7d981e103c2e955ad86fdc8138a5030968
TanLeSi <tan.lesi@study.hs-duesseldorf.de>
2022-09-29 11:57:59 +0200

    update setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-mkdir -p .streamlit
-echo -e "\
+rm ~/.streamlit/config.toml
+mkdir ./.streamlit/
+echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
 \n\
-" > .streamlit/config.toml
+" > ./.streamlit/config.toml
